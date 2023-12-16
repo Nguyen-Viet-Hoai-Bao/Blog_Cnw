1eb57550a8968ecaaaca1a3e2da3e190a20345c5
Nguyen-Viet-Hoai-Bao <107166813+Nguyen-Viet-Hoai-Bao@users.noreply.github.com>
2023-12-16 20:09:50 +0700

    Update Db2.java

--- src/com/blog/dao/Db2.java
+++ src/com/blog/dao/Db2.java
@@ -24,30 +24,13 @@ public class Db2 {
 		for (Blog map : select) {
 			System.out.println(map);
 		}
-//		String user = "坤旭";
-//		String pwd = "123456";
-//		System.out.println(user);
-//		System.out.println(pwd);
-//		String sqlInsert = "insert into t_user (userName,pwd) values('" + user + "','" + pwd + "')";
-//		boolean insert = insert(sqlInsert);
-//		if(insert) {
-//			System.out.println("插入成功");
-//		}else {
-//			System.out.println("插入失败");
-//		}
-//		String sql = "select count(*) from t_user";
-//		int count = getCount(sql);
-//		System.out.println(count);
 	}
 
 	public static boolean insert(String sql) {
 		int executeUpdate = 0;
 		try {
-			// 创建连接
 			Connection conn = DbConnection.getConnection();
-			// 创建一个"小手"
 			Statement statement = conn.createStatement();
-			// 执行插入语句
 			executeUpdate = statement.executeUpdate(sql);
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -55,21 +38,12 @@ public class Db2 {
 		return executeUpdate > 0;
 	}
 
-	/**
-	 * 
-	 * @param sql  查询的sql select * from user where userName='zhangsan'
-	 * @param args 查询的列名 数组
-	 * @return 对应的数据
-	 */
 	public static List<Map<String, String>> select(String sql, String[] args) {
 		List<Map<String, String>> list = new ArrayList<>();
 		try {
 			Connection conn = DbConnection.getConnection();
-			// 创建一个"小手"
 			Statement statement = conn.createStatement();
-			// 通过我们这个小手执行我们的sql拿回数据
 			ResultSet resultSet = statement.executeQuery(sql);
-			// 遍历结果集
 			while (resultSet.next()) {
 				Map<String, String> map = new HashMap<>();
 				for (String str : args) {
@@ -83,22 +57,12 @@ public class Db2 {
 		return list;
 	}
 
-	/**
-	 * 
-	 * @param      <T>
-	 * @param sql  查询的sql select * from user where userName='zhangsan'
-	 * @param args 查询的列名 数组
-	 * @return 对应的数据
-	 */
 	public static <T> List<T> select(String sql, Class<T> cl) {
 		List<T> list = new ArrayList<>();
 		try {
 			Connection conn = DbConnection.getConnection();
-			// 创建一个"小手"
 			Statement statement = conn.createStatement();
-			// 通过我们这个小手执行我们的sql拿回数据
 			ResultSet resultSet = statement.executeQuery(sql);
-			// 遍历结果集
 			while (resultSet.next()) {
 				T t = cl.newInstance();
 //				String [] strs= {};
@@ -120,23 +84,12 @@ public class Db2 {
 		}
 		return list;
 	}
-
-	/**
-	 * 
-	 * @param      <T>
-	 * @param sql  查询的sql select * from user where userName='zhangsan'
-	 * @param args 查询的列名 数组
-	 * @return 对应的数据
-	 */
 	public static <T> List<T> select1(String sql, Class<T> cl) {
 		List<T> list = new ArrayList<>();
 		try {
 			Connection conn = DbConnection.getConnection();
-			// 创建一个"小手"
 			Statement statement = conn.createStatement();
-			// 通过我们这个小手执行我们的sql拿回数据
 			ResultSet resultSet = statement.executeQuery(sql);
-			// 遍历结果集
 			while (resultSet.next()) {
 				T t = cl.newInstance();
 //				String [] strs= {};
@@ -182,16 +135,6 @@ public class Db2 {
 		return String.valueOf(chars);
 
 	}
-
-	// 一个sql
-	// int
-	/**
-	 * 通过传入一个sql 获得总条数
-	 * 
-	 * @param sql select count(1) from t_user
-	 * @return 总条数
-	 * @author yhtgb
-	 */
 	public static int getCount(String sql) {
 		Connection connection = DbConnection.getConnection();
 		int total = 0;
